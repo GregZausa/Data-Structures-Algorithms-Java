2308ae53d342d0c11687f396a0cc7779078d708f
GregZausa <gregclarence.zausa@gmail.com>
2025-07-24 13:31:40 +0800

    Initial commit: Added Eclipse config and .gitignore

---
+++ DataStructuresAndAlgorithms/src/DSA/Main.java
@@ -0,0 +1,13 @@
+package DSA;
+import java.util.*;
+public class Main {
+
+	public static void main(String[] args) {
+		int [] nums = {4, 2, 1, 5};
+		int target = 9;
+		System.out.print(TwoSum.twoSum(nums, target));
+		
+		
+	}
+
+}
